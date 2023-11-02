915a3a4a25bee630cf0b6056aebf20827e1beef7
Patrikas Gabrielius Ionescu <patrickgabrielionescu@gmail.com>
2023-11-02 15:58:41 +0100

    made an object for running scripts for future compatibility

--- Data.py
+++ Data.py
@@ -1,17 +1,14 @@
 from bs4 import BeautifulSoup as bs
 
 class Data():
-    def __init__(self, source: str, box):
-        self.box = box[0]
-        self.soup = bs(source, 'html.parser')
+    def __init__(self, box: str):
+        self.box = bs(box, 'html.parser')
+
     def get_nonce(self) -> str:
-        nonce = self.box.find_all('span', {'class': ''}).text
-
-        return nonce
-
+        return self.box.script['nonce']
 
     def prettify(self):
-        return self.box.apply(lambda x : bs(x, 'html.parser').prettify())
+        return self.box.apply(lambda x : self.box.prettify())
         
     def save_data(self):
         pass
--- main.py
+++ main.py
@@ -3,24 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from Data import Data
 
-url = 'https://developers.facebook.com/docs/plugins/page-plugin/'
-driver = webdriver.Chrome()
-driver.get(url)
-html = driver.page_source
-button = driver.find_element('css selector', '._42ft._4jy0._9xo6._4jy3._4jy1.selected._51sy')
-button.click()
-search = driver.find_element('css selector', '.inputtext._55r1._55r2.inputtext.wideinputtext')
-search.clear()
-search.send_keys('https://www.facebook.com/jessicaShywouh')
-# For some reason, this won't work if you don't use By object and specify FullXPATH
-button = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[5]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/span/div/div[2]/div/div[3]/a")
-sleep(2)
-button.click()
-sleep(10)
-script_box = driver.find_elements('css selector', '.prettyprint._3t10.prettyprinted')
-script = script_box[0].text
-print(type(script))
-
-data = Data(html, script)
-data.get_nonce()
-driver.quit()
+class Nonce:
+    def __init__(self, url: str, page_url: str):
+        self.url = url
+        self.page_url = page_url
+    
+    def run(self):
+        driver = webdriver.Chrome()
+        driver.get(self.url)
+        button = driver.find_element('css selector', '._42ft._4jy0._9xo6._4jy3._4jy1.selected._51sy')
+        button.click()
+        search = driver.find_element('css selector', '.inputtext._55r1._55r2.inputtext.wideinputtext')
+        search.clear()
+        search.send_keys(self.page_url)
+        # For some reason, this won't work if you don't use By object and specify FullXPATH
+        button = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[5]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/span/div/div[2]/div/div[3]/a")
+        sleep(1)
+        button.click()
+        sleep(1)
+        script = driver.find_elements('css selector', '.prettyprint._3t10.prettyprinted').text
+        driver.quit()
+        data = Data(script)
+        return data.get_nonce()
